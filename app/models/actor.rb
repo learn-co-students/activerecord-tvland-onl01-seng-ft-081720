e1e3f6a4584909212fc842317ebd037754174d2a
Eli Cusic <tntitans31@gmail.com>
2020-09-22 08:57:08 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.map{|char| "#{char.name} - #{Show.find(char.show_id).name}"}
+    end
 end
