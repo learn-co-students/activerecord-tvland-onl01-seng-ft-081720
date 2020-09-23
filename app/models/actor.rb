8fdebd7843ee48f70757d8cd11fea48e69800c60
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-09-23 03:56:56 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :shows, through: :characters 
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        result=""
+        self.characters.each {|char| result+= "#{char.name} - "}
+        self.shows.each {|show| result += show.name}
+        result.split("**")
+    end
 end
