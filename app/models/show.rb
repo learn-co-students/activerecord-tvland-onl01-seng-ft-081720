924d7b50dad20c2ad61164d6c2979bdbdf6a60f8
Richard Kurwitz <kurwitz3@aol.com>
2020-09-25 01:42:34 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many  :characters 
+  has_many :actors, through: :characters
+  belongs_to :network 
+  def actors_list 
+    x = [] 
+    Character.select  do |m|
+     x <<  "#{m.actor.first_name} #{ m.actor.last_name}"
+    #binding.pry 
+ end
+ x
+end
 end
